a56f28a6d18a1fccae089c5089ecd38868c94465
Kilien <kilienazure@gmail.com>
2025-06-18 18:30:09 +0800

    fix:header

--- app/components/Header/Menu.tsx
+++ app/components/Header/Menu.tsx
@@ -1,41 +1,59 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import { MdClose } from 'react-icons/md';
 
-interface MenuProps {
-  show: boolean;
-  onClose: () => void;
+interface MenuItem {
+  name: string;
+  href: string;
+  id: string;
+  icon: string;
+  offset: number;
 }
 
-const Menu = ({ show, onClose }: MenuProps) => {
-  return (
-    <div
-      className={`fixed top-0 left-0 z-50 h-full w-full bg-black pt-20 text-white transition-transform duration-300 ${show ? 'translate-x-0' : '-translate-x-full'} `}
-      style={{ willChange: 'transform' }}
-    >
-      {/* Logo */}
-      <img src="/logo.png" alt="logo" className="mx-auto mb-[46px] h-6" />
-      {/* 菜单项 */}
-      <nav className="flex w-full flex-col items-center gap-8">
-        <a href="/" className="text-lg font-medium hover:text-blue-400">
-          Home
-        </a>
-        <a href="/" className="text-lg font-medium hover:text-blue-400">
-          AI Agent
-        </a>
-        <a href="/" className="text-lg font-medium hover:text-blue-400">
-          Product
-        </a>
-        <a href="/" className="text-lg font-medium hover:text-blue-400">
-          Docs
-        </a>
-      </nav>
-      {/* Connect Wallet 按钮 */}
+interface MenuProps {
+  tableList: MenuItem[];
+  onMenuClick: (item: MenuItem) => void;
+}
 
-      {/* 关闭按钮 */}
-      <MdClose
-        className="absolute bottom-4 left-1/2 -translate-x-1/2 cursor-pointer text-2xl text-white"
-        onClick={onClose}
-      />
+const Menu = ({ tableList, onMenuClick }: MenuProps) => {
+  return (
+    <div className="drawer-side z-[9999]">
+      <label htmlFor="mobile-drawer" className="drawer-overlay"></label>
+
+      {/* Menu Content */}
+      <aside className="min-h-screen w-screen bg-black pt-20 text-white">
+        {/* Logo */}
+        <img src="/logo.png" alt="logo" className="mx-auto mb-[46px] h-20" />
+
+        {/* 菜单项 */}
+        <nav className="flex w-full flex-col items-center gap-4">
+          {tableList.map(item => (
+            <div
+              onClick={() => onMenuClick(item)}
+              key={item.name}
+              className="flex cursor-pointer items-center justify-center gap-2"
+            >
+              {item?.icon && (
+                <img
+                  src={`/imgs/common/${item.icon}`}
+                  alt={item.name}
+                  className="mb-1 h-[18px]"
+                />
+              )}
+              <span className="text-2xl text-white">{item.name}</span>
+            </div>
+          ))}
+        </nav>
+
+        {/* Connect Wallet 按钮 */}
+
+        {/* 关闭按钮 */}
+        <label
+          htmlFor="mobile-drawer"
+          className="absolute bottom-4 left-1/2 -translate-x-1/2 cursor-pointer"
+        >
+          <MdClose className="text-2xl text-white" />
+        </label>
+      </aside>
     </div>
   );
 };
--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -1,75 +1,229 @@
 import { useMediaQuery } from '~/hooks/useMediaQuery';
 import { RxTextAlignJustify } from 'react-icons/rx';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { ConnectButton, useConnectModal } from '@rainbow-me/rainbowkit';
 import Menu from './Menu';
-import { useConnectModal } from '@rainbow-me/rainbowkit';
 
 const Header = () => {
   const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
   const [showMenu, setShowMenu] = useState(false);
   const { openConnectModal } = useConnectModal();
+  const navigate = useNavigate();
 
   const tableList = [
     {
       name: 'Home',
       href: '/',
+      id: '/',
+      icon: '',
+      offset: 0,
     },
     {
-      name: 'AI Agent',
+      name: 'Roadmap',
       href: '/',
-    },
-    {
-      name: 'Product',
-      href: '/',
+      id: 'roadmap',
+      icon: '',
+      offset: 4000,
     },
     {
       name: 'Docs',
       href: '/',
+      id: 'doc',
+      icon: '',
+      offset: 5800,
     },
   ];
 
+  const handleClickMenu = (item: {
+    name: string;
+    href: string;
+    id: string;
+    offset?: number;
+  }) => {
+    if (item.href !== '/') {
+      window.location.href = item.href;
+    } else if (item.id !== '/') {
+      const element = document.getElementById(item.id);
+      if (element) {
+        const elementPosition = element.getBoundingClientRect().top;
+        const offsetPosition = elementPosition + window.pageYOffset;
+
+        window.scrollTo({
+          top: item.offset,
+          behavior: 'smooth',
+        });
+      }
+    }
+    setShowMenu(false); // 关闭菜单
+  };
+
+  const handleDesktopClickMenu = (item: {
+    name: string;
+    href: string;
+    id: string;
+  }) => {
+    if (item.href) {
+      if (item.id !== '/') {
+        const element = document.getElementById(item.id);
+        if (element) {
+          element.scrollIntoView({ behavior: 'smooth' });
+        }
+      }
+      navigate(item.href);
+    }
+  };
+
   return (
-    <div className="absolute top-0 right-0 left-0 z-10 mt-6 flex w-full items-center justify-between px-[16px] md:mt-[60px] lg:px-[64px] 2xl:px-[169px]">
-      <img
-        src="/logo.png"
-        alt="logo"
-        className="mr-8 h-6 object-contain md:h-10 xl:mr-[96px]"
+    <div className="drawer">
+      <input
+        id="mobile-drawer"
+        type="checkbox"
+        className="drawer-toggle"
+        checked={showMenu}
+        onChange={e => setShowMenu(e.target.checked)}
       />
 
-      {!isMobile && (
-        <div className="flex flex-1 items-center justify-start gap-[40px] xl:gap-[80px]">
-          {tableList.map(item => (
-            <a
-              href={item.href}
-              key={item.name}
-              className="text-base text-white"
+      <div className="drawer-content">
+        {/* Header Content */}
+        <div className="absolute top-0 right-0 left-0 z-10 flex h-[80px] w-full items-center justify-between px-[16px] backdrop-blur-[10px] md:bg-[#00000080] lg:px-[48px] 2xl:px-[169px]">
+          <img
+            src="/logo.png"
+            alt="logo"
+            className="mr-8 h-14 object-contain md:h-14 xl:mr-14"
+          />
+
+          {!isMobile && (
+            <div className="flex flex-1 items-center justify-start gap-8 xl:gap-10">
+              {tableList.map(item => (
+                <div
+                  onClick={() => handleDesktopClickMenu(item)}
+                  key={item.name}
+                  className="flex cursor-pointer items-center justify-center gap-2"
+                >
+                  {item?.icon && (
+                    <img
+                      src={`/imgs/common/${item.icon}`}
+                      alt={item.name}
+                      className="mb-1 h-[18px]"
+                    />
+                  )}
+                  <span className="text-2xl text-white">{item.name}</span>
+                </div>
+              ))}
+            </div>
+          )}
+
+          {isMobile ? (
+            <label
+              htmlFor="mobile-drawer"
+              className="drawer-button cursor-pointer"
             >
-              {item.name}
-            </a>
-          ))}
+              <RxTextAlignJustify
+                color="#fff"
+                size={24}
+                className="h-[24px] w-[24px]"
+              />
+            </label>
+          ) : (
+            <ConnectButton.Custom>
+              {({
+                account,
+                chain,
+                openAccountModal,
+                openChainModal,
+                openConnectModal,
+                authenticationStatus,
+                mounted,
+              }) => {
+                const ready = mounted && authenticationStatus !== 'loading';
+                const connected =
+                  ready &&
+                  account &&
+                  chain &&
+                  (!authenticationStatus ||
+                    authenticationStatus === 'authenticated');
+
+                return (
+                  <div
+                    {...(!ready && {
+                      'aria-hidden': true,
+                      style: {
+                        opacity: 0,
+                        pointerEvents: 'none',
+                        userSelect: 'none',
+                      },
+                    })}
+                  >
+                    {(() => {
+                      if (!connected) {
+                        return (
+                          <button
+                            className="flex h-[72px] w-[180px] items-center justify-center"
+                            style={{
+                              backgroundImage: 'url(/imgs/common/play-btn.png)',
+                              backgroundSize: '100% 100%',
+                              backgroundPosition: 'center',
+                              backgroundRepeat: 'no-repeat',
+                            }}
+                            type="button"
+                            onClick={openConnectModal}
+                          >
+                            <span className="text-center text-xl font-normal text-white drop-shadow-[0_0_6px_#7DFF65]">
+                              Connect Wallet
+                            </span>
+                          </button>
+                        );
+                      }
+
+                      if (chain.unsupported) {
+                        return (
+                          <button
+                            className="flex h-[72px] w-[180px] items-center justify-center"
+                            style={{
+                              backgroundImage: 'url(/imgs/common/play-btn.png)',
+                              backgroundSize: '100% 100%',
+                              backgroundPosition: 'center',
+                              backgroundRepeat: 'no-repeat',
+                            }}
+                            type="button"
+                            onClick={openChainModal}
+                          >
+                            <span className="text-center text-xl font-normal text-white drop-shadow-[0_0_6px_#7DFF65]">
+                              ErrorNet
+                            </span>
+                          </button>
+                        );
+                      }
+
+                      return (
+                        <button
+                          className="flex h-[72px] w-[180px] items-center justify-center"
+                          style={{
+                            backgroundImage: 'url(/imgs/common/play-btn.png)',
+                            backgroundSize: '100% 100%',
+                            backgroundPosition: 'center',
+                            backgroundRepeat: 'no-repeat',
+                          }}
+                          type="button"
+                          onClick={openAccountModal}
+                        >
+                          <span className="text-center text-xl font-normal text-white drop-shadow-[0_0_6px_#7DFF65]">
+                            {account.displayName}
+                          </span>
+                        </button>
+                      );
+                    })()}
+                  </div>
+                );
+              }}
+            </ConnectButton.Custom>
+          )}
         </div>
-      )}
+      </div>
 
-      {isMobile ? (
-        <RxTextAlignJustify
-          color="#fff"
-          size={24}
-          className="h-[24px] w-[24px] cursor-pointer"
-          onClick={() => setShowMenu(true)}
-        />
-      ) : (
-        <button
-          className="h-[60px] w-[240px] cursor-pointer rounded-full text-base text-white"
-          style={{
-            background: 'linear-gradient(90deg, #5694FF 0%, #2659FF 100%)',
-          }}
-          onClick={() => openConnectModal?.()}
-        >
-          Connect Wallet
-        </button>
-      )}
-
-      {showMenu && <Menu show={showMenu} onClose={() => setShowMenu(false)} />}
+      {/* 使用 Menu 组件 */}
+      <Menu tableList={tableList} onMenuClick={handleClickMenu} />
     </div>
   );
 };
